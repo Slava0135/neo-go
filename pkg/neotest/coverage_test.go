09b5dd0fed3c33ebd6bc5c649ae1627a5858c5fc
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-09-06 11:48:42 +0300

    test for block overlap

--- pkg/neotest/coverage_test.go
+++ pkg/neotest/coverage_test.go
@@ -98,3 +98,95 @@ func TestProcessCover_TwoMethodsTwoDocuments(t *testing.T) {
 	require.Contains(t, documentCover2, coverBlock{startLine: 1, endLine: 1, stmts: 1, counts: 1})
 	require.Contains(t, documentCover2, coverBlock{startLine: 2, endLine: 2, stmts: 1, counts: 1})
 }
+
+func TestProcessCover_BlockOverlap(t *testing.T) {
+	testCases := []struct {
+		name string
+		mdi  compiler.MethodDebugInfo
+	}{
+		{
+			name: "Different lines",
+			mdi: compiler.MethodDebugInfo{
+				SeqPoints: []compiler.DebugSeqPoint{
+					{Opcode: 0, Document: 0, StartLine: 2, EndLine: 9},
+					{Opcode: 1, Document: 0, StartLine: 1, EndLine: 10},
+				},
+			},
+		},
+		{
+			name: "Different start lines",
+			mdi: compiler.MethodDebugInfo{
+				SeqPoints: []compiler.DebugSeqPoint{
+					{Opcode: 0, Document: 0, StartLine: 2, EndLine: 9},
+					{Opcode: 1, Document: 0, StartLine: 1, EndLine: 9},
+				},
+			},
+		},
+		{
+			name: "Different end lines",
+			mdi: compiler.MethodDebugInfo{
+				SeqPoints: []compiler.DebugSeqPoint{
+					{Opcode: 0, Document: 0, StartLine: 2, EndLine: 9},
+					{Opcode: 1, Document: 0, StartLine: 2, EndLine: 10},
+				},
+			},
+		},
+		{
+			name: "Different columns",
+			mdi: compiler.MethodDebugInfo{
+				SeqPoints: []compiler.DebugSeqPoint{
+					{Opcode: 0, Document: 0, StartCol: 2, EndCol: 9},
+					{Opcode: 1, Document: 0, StartCol: 1, EndCol: 10},
+				},
+			},
+		},
+		{
+			name: "Different start columns",
+			mdi: compiler.MethodDebugInfo{
+				SeqPoints: []compiler.DebugSeqPoint{
+					{Opcode: 0, Document: 0, StartCol: 2, EndCol: 9},
+					{Opcode: 1, Document: 0, StartCol: 1, EndCol: 9},
+				},
+			},
+		},
+		{
+			name: "Different end columns",
+			mdi: compiler.MethodDebugInfo{
+				SeqPoints: []compiler.DebugSeqPoint{
+					{Opcode: 0, Document: 0, StartCol: 2, EndCol: 9},
+					{Opcode: 1, Document: 0, StartCol: 2, EndCol: 10},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Cleanup(resetCoverage)
+
+			scriptHash := util.Uint160{1}
+			doc := "foobar.go"
+			di := &compiler.DebugInfo{
+				Documents: []string{doc},
+				Methods:   []compiler.MethodDebugInfo{tc.mdi},
+			}
+			contract := &Contract{Hash: scriptHash, DebugInfo: di}
+		
+			addScriptToCoverage(contract)
+			coverageHook(scriptHash, 0, opcode.NOP)
+			coverageHook(scriptHash, 1, opcode.NOP)
+			cover := processCover()
+		
+			require.Contains(t, cover, doc)
+			documentCover := cover[doc]
+			require.Equal(t, 1, len(documentCover))
+			expectedBlock := tc.mdi.SeqPoints[0]
+			actualBlock := documentCover[0]
+			require.Equal(t, actualBlock.startLine, expectedBlock.StartLine)
+			require.Equal(t, actualBlock.endLine, expectedBlock.EndLine)
+			require.Equal(t, actualBlock.startCol, expectedBlock.StartCol)
+			require.Equal(t, actualBlock.endCol, expectedBlock.EndCol)
+			require.Equal(t, actualBlock.counts, 1)
+		})
+	}
+}
