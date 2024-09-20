30b99d64fa5c411e3284a5523875d7be631256cd
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-09-20 13:36:46 +0300

    parsing argument as base64

--- cli/main.go
+++ cli/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
 	"os"
-
-	"github.com/nspcc-dev/neo-go/cli/app"
 )
 
 func main() {
-	ctl := app.New()
-
-	if err := ctl.Run(os.Args); err != nil {
-		fmt.Fprintln(ctl.ErrWriter, err)
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stderr, "expected string (base64) as argument\n")
 		os.Exit(1)
 	}
+	script, err := base64.StdEncoding.DecodeString(string(args[0]))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error when decoding string (base64): %s\n", err)
+		os.Exit(2)
+	}
+	fmt.Printf("%v\n", script)
 }
