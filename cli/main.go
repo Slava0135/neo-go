e0af25c53c03fc516a57d79a3a632df3f02b6a91
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-09-20 13:48:40 +0300

    run scripts in vm

--- cli/main.go
+++ cli/main.go
@@ -4,18 +4,43 @@ import (
 	"encoding/base64"
 	"fmt"
 	"os"
+
+	"github.com/nspcc-dev/neo-go/pkg/vm"
+)
+
+const (
+	vmHaltedCode    = 0
+	wrongArgCode    = 1
+	wrongStringCode = 2
+	runErrorCode    = 3
+	vmFailedCode    = 4
 )
 
 func main() {
 	args := os.Args[1:]
 	if len(args) != 1 {
 		fmt.Fprintf(os.Stderr, "expected string (base64) as argument\n")
-		os.Exit(1)
+		os.Exit(wrongArgCode)
 	}
 	script, err := base64.StdEncoding.DecodeString(string(args[0]))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error when decoding string (base64): %s\n", err)
-		os.Exit(2)
+		os.Exit(wrongStringCode)
 	}
-	fmt.Printf("%v\n", script)
+	vm := vm.New()
+	vm.LoadScript(script)
+	err = vm.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error when running script: %s\n", err)
+		os.Exit(runErrorCode)
+	}
+	switch {
+	case vm.HasFailed():
+		fmt.Println("result: VM failed")
+		os.Exit(vmFailedCode)
+	case vm.HasHalted():
+		fmt.Println("result: VM halted")
+		fmt.Println(vm.DumpEStack())
+		os.Exit(vmHaltedCode)
+	}
 }
